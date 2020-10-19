63d55cee6b9366e9ed21f455273a5a7be99d150d
Daniele Laudani <s280077@studenti.polito.it>
2020-10-19 14:40:53 +0200

    code: fix ticket selection algorithm

--- server/countersDao.js
+++ server/countersDao.js
@@ -13,12 +13,12 @@ exports.getCounters = function() {
 exports.getCounter = function(id) {
     return new Promise((resolve, reject) => {
         let sql = "SELECT DISTINCT s.id, s.name FROM counters c, service_types s, counters_service_types cs WHERE cs.counter_id = ? and s.id = cs.service_type_id";
-        const service_types = db.prepare(sql).all(id)
+        const serviceTypes = db.prepare(sql).all(id)
         sql = "SELECT * FROM counters WHERE id = ?"
         const counter = db.prepare(sql).get(id)
         sql = "SELECT id FROM tickets WHERE counter_id = ?"
         const ticket = db.prepare(sql).get(id)
-        resolve({'counter':counter, 'current_ticket':ticket, 'service_types':service_types})
+        resolve({'counterId':counter.id, 'counterName':counter.name, 'currentTicketId':ticket, 'serviceTypes':serviceTypes})
     })
 }
 
@@ -27,9 +27,15 @@ exports.getNewTicketToServe = function(counterId) {
         let sql = "DELETE FROM tickets WHERE counter_id = ?"
         db.prepare(sql).run(counterId)
         
-        sql = "select id, service_type_id, count(*) as tot from tickets where counter_id is null group by service_type_id order by count(*) DESC"
-        //TODO: if tot is equal, select the service type with the smallest service time
-        const rows = db.prepare(sql).all()[0]
+        sql = "select t.id, t.service_type_id, s.service_time, count(*) as tot from tickets t, service_types s \
+            where t.counter_id is null and t.service_type_id = s.id and t.service_type_id in ( \
+                    SELECT DISTINCT s.id FROM counters c, service_types s, counters_service_types cs \
+                    WHERE cs.counter_id = ? and s.id = cs.service_type_id \
+            ) group by t.service_type_id order by count(*) desc, service_time asc"
+        const rows = db.prepare(sql).all(counterId)[0]
+        
+        if(!rows)
+            reject("No tickets to serve from this counter")
         
         sql = "UPDATE tickets SET counter_id = ? WHERE id = ?"
         db.prepare(sql).run([counterId, rows.id])
