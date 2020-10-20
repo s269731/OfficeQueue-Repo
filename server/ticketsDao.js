3c32be18a5a21e44912f19ab05c59021c7d46c56
Nino <s267906@studenti.polito.it>
2020-10-20 11:01:15 +0200

    computeWaitingTime completed

--- server/ticketsDao.js
+++ server/ticketsDao.js
@@ -3,20 +3,51 @@
 const db = require('./db')
 
 
+
 function computeWaitingTime(serviceTypeId) {
-    //TODO
-    return Math.floor(Math.random() * 30)
+    console.log("serviceTypeId passed as parameter to computeWaitingTime: "+serviceTypeId)
+    const sql ="SELECT counter_id FROM counters_service_types WHERE service_type_id=?" //get counterIDs that performs that service type
+    //let arrayCounterIDservices
+    let DifferentServicesSum=0
+    const stmt = db.prepare(sql)
+    const counterIdArray = stmt.all([serviceTypeId])
+    counterIdArray.forEach(element=>{
+        const sql ="SELECT COUNT(*) as value FROM counters_service_types WHERE counter_id=?"
+        const stmt=db.prepare(sql)
+        const res = stmt.get([element.counter_id])
+        DifferentServicesSum=DifferentServicesSum+1/(res.value)
+    })
+
+    const nPeopleInQueue = getPeopleNumberInQueue(serviceTypeId)
+    const sql1="SELECT service_time FROM service_types WHERE id=?"
+    const stmt1 = db.prepare(sql1)
+    let serviceTime = stmt1.get([serviceTypeId])
+
+    return serviceTime.service_time *(nPeopleInQueue/DifferentServicesSum+1/2)
+}
+
+
+function getPeopleNumberInQueue(serviceTypeId){
+        console.log("serviceTypeId passed as parameter to getPeopleNumberInQueue: "+serviceTypeId)
+        const sql="SELECT COUNT(*) as value FROM tickets WHERE counter_id IS NULL AND service_type_id=?"
+        const stmt = db.prepare(sql)
+        const res = stmt.get([serviceTypeId])
+        console.log("res.value in getPeopleNumberInQueue:"+res.value)
+        return res.value
+
 }
 
 exports.addTicket = function(serviceTypeId) {
+    console.log("serviceTypeId passed as parameter to addTicket: "+serviceTypeId)
     return new Promise((resolve, reject) => {
         const waitingTime = computeWaitingTime(serviceTypeId)
-        
-        serviceTypeId = Math.floor(Math.random() * 3) + 1
-        
+
         const sql = "INSERT INTO tickets(service_type_id, waiting_time) VALUES(?, ?)"
         const stmt = db.prepare(sql)
         const res = stmt.run([serviceTypeId, waitingTime])
-        resolve({serviceTypeId:serviceTypeId, waitingTime:waitingTime})
+        //const res = stmt.run([serviceTypeId, 5])
+        resolve({"ticketId":res.lastInsertRowid, "serviceTypeId":serviceTypeId, "waitingTime":waitingTime})
+        //resolve({"ticketId":res.lastInsertRowid, "serviceTypeId":serviceTypeId, "waitingTime":5})
+
     })
 }
