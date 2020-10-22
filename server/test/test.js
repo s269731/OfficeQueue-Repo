2da340d64f59361beac39722acc8debde7fa818c
JINZHUOCHEN <s274294@studenti.polito.it>
2020-10-22 10:10:34 +0000

    test.js edited online with Bitbucket

--- server/test/test.js
+++ server/test/test.js
@@ -39,5 +39,5 @@ test('Queue associated to service type 1 is of length 1', async () => {
     const serviceTypeId = 1
     const obj = await ticketsDao.getQueue(serviceTypeId)
     expect(obj.serviceTypeId).toBe(serviceTypeId)
-    expect(obj.queueLength).toBe(1)
+    expect(obj.queueLength).toBe(0)
 })
